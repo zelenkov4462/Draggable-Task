f1e6dd49c75d87c4069f4411691188f2a5343abb
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-11-29 08:27:34 +0700

    Draggable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+import { randomColor } from "randomcolor";
+import Draggable from "react-draggable";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("items")) || []
+  );
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  const changeHandler = (e) => {
+    setItem(e.target.value);
+  };
+
+  const getRandomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  const addHandler = () => {
+    if (item.trim() === "") return;
+    setItems([
+      ...items,
+      {
+        id: uuid(),
+        item,
+        color: randomColor({
+          luminosity: "light",
+        }),
+        defaultPos: {
+          x: getRandomNumber(0, width - 215),
+          y: -getRandomNumber(0, height - 50),
+        },
+      },
+    ]);
+    setItem("");
+  };
+
+  const deleteHandler = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  const updatePos = (data, index) => {
+    let newArray = [...items];
+    newArray[index].defaultPos = { x: data.x, y: data.y };
+    setItems(newArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="wrapper">
+        <input
+          value={item}
+          onChange={changeHandler}
+          type="text"
+          placeholder="Enter your tasks..."
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              addHandler();
+            }
+          }}
+        />
+        <button onClick={addHandler} className="enter">
+          ENTER
+        </button>
+      </div>
+      {items.map((item, index) => (
+        <Draggable
+          key={index}
+          defaultPosition={item.defaultPos}
+          onStop={(_, data) => {
+            updatePos(data, index);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="todo__item" style={{ backgroundColor: item.color }}>
+            <div className={"item__title"}> {`${item.item}`}</div>
+            <button className="delete" onClick={() => deleteHandler(item.id)}>
+              X
+            </button>
+          </div>
+        </Draggable>
+      ))}
     </div>
   );
 }
